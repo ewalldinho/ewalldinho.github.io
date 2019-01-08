41c24f32998ee94b71b1b5181828699668ea11d4
ewalldinho <39264325+ewalldinho@users.noreply.github.com>
2019-01-08 11:20:13 +0200

    Created crazy villain action

---
+++ notagame/ai/villain_crazy.c
@@ -0,0 +1,11 @@
+#incluce<villain.c>
+
+void act_crazy(villain *v, int * par_array, char * words) {
+  if (words != 0) {
+    saySomething(v, words);
+  }
+  
+  // calculate from params
+  // do action with villain
+
+}
