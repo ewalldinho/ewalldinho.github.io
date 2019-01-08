cc5950b995470d8bd0479171deb459d50c2507ce
ewalldinho <39264325+ewalldinho@users.noreply.github.com>
2019-01-08 13:22:23 +0200

    Added some logic to action

--- notagame/ai/villain_crazy.c
+++ notagame/ai/villain_crazy.c
@@ -7,5 +7,8 @@ void act_crazy(villain *v, int * par_array, char * words) {
   
   // calculate from params
   // do action with villain
-
+  while (true) {
+    // check for a condition to break;
+    // break;
+  }
 }
